17f62bd19488180dc1c74a81b93d03461a8c5b35
Nathan Akins <luigi9907.na@gmail.com>
2026-09-09 21:20:48 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Nathan Moore Akins
+* Course: CSCI 1250, Section 002
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Your Full Name.");
+Console.WriteLine("This is my first program at ETSU.");
